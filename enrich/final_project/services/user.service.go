2b572c88b91e98a4b1977e2e6af37dea53f81a6f
kunkka079x <nobita079x@gmail.com>
2022-10-23 02:21:33 +0900

    remove temp

--- enrich/final_project/services/user.service.go
+++
@@ -1,8 +0,0 @@
-package services
-
-import "github.com/kunkka19xx/enrich-golang/models"
-
-type UserService interface {
-	FindUserById(string) (*models.DBResponse, error)
-	FindUserByEmail(string) (*models.DBResponse, error)
-}
